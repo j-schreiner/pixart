e029f33b3be764cb9d81ad02d4d0578996f224d6
Johanna Schreiner <jschreinerdesign@gmail.com>
2018-02-18 14:12:22 +1100

    commit 4: click to make box green

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 $('.color-choice').keypress(function(event){
-
   var keycode = event.keyCode || event.which
 
   if(keycode == '13') {
-
     $colorChoice = $('.color-choice').val()
-
     $('.brush').css("background-color", $colorChoice )
-
     event.preventDefault()
   }
 })
 
 
-
 for (var i = 0; i <= 100; i++){
-
-  var newSquareDiv = $('<div>', {
-  "class": "square",
-  click: function(event){
-    event.css("background-color", "green" )
-    }
-  })
-
-  $('body').append(newSquareDiv)
-
+  var $newSquareDiv = $('<div>', {"class": "square"})
+  $('.squares').append($newSquareDiv)
 }
+
+
+
+$('.squares').on('click', '.square', function(event){
+  $(event.target).css("background-color", "green")
+})
+
